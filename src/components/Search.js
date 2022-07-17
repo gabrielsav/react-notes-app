e81619940bdc73b4bf5929b84d01798eeacbd1d4
gabrielsav <gscontatoec@gmail.com>
2022-07-17 03:02:10 -0300

    feat: localstorage keep user notes (inprivate) style: var colors

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,19 @@
-import React from "react";
-import { MdSearch } from "react-icons/md";
+import React from 'react';
+import { MdSearch } from 'react-icons/md';
 
-const Search = ({handleSearchNote}) => {
-    return <div className="search">
-    <MdSearch className="search-icons" size="1.3em" />
-    <input 
-        onChange={(event) =>
-        handleSearchNote(event.target.value)
-        } 
-        type='text' 
-        placeholder="Search your notes..." />
-    </div>
+const Search = ({ handleSearchNote }) => {
+	return (
+		<div className='search'>
+			<MdSearch className='search-icons' size='1.3em' />
+			<input
+				onChange={(event) =>
+					handleSearchNote(event.target.value)
+				}
+				type='text'
+				placeholder='Search your notes...'
+			/>
+		</div>
+	);
 };
 
 export default Search;
